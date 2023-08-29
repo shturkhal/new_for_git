652dc76511115f033d7ac3cffeb858a5449b1e48
Ivan Shturkhal <ivshtur@icloud.com>
2023-08-29 21:14:19 +0300

    Initial Commit

---
+++ new_for_git/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  new_for_git
+//
+//  Created by Іван Штурхаль on 29.08.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
